f9914ec0f9d9afe47ecc9b3e2a616a057c1f88ab
creedboy <polozh@naver.com>
2023-08-13 23:13:23 +0900

    jpaconfig 적용

--- src/main/java/com/creedboy/springbootboard/domain/Article.java
+++ src/main/java/com/creedboy/springbootboard/domain/Article.java
@@ -1,21 +1,61 @@
 package com.creedboy.springbootboard.domain;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
 @ToString
-//@Table(index)
+@Table(indexes = {
+    @Index(columnList = "title"),
+    @Index(columnList = "hashTag"),
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title; // 제목
+
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content; // 내용
+
+    @Setter
     private String hashTag; // 해시태그
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt; // 생성일시
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy; // 생성자
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일시
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy; // 수정자
 }
