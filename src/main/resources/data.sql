7d8bdce218ee5742a61d3580781e8d355126c1ed
creedboy <polozh@naver.com>
2023-08-16 23:02:25 +0900

    querydsl 의존성 및 설정 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,9 +12,9 @@
 
 -- 123 게시글
 insert into article (user_id, title, content, created_by, modified_by, created_at, modified_at, hashtag)
-values ('creedboy1', '제목입니다.1', '내용입니다.2', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50', '해시태그'),
-       ('creedboy2', '제목입니다.1', '내용입니다.2', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54', '해시태그');
+values ('creedboy1', '제목입니다.11', '내용입니다.11', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50', '해시태그11'),
+       ('creedboy2', '제목입니다.22', '내용입니다.222', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54', '해시태그22');
 
 insert into article_comment (article_id, content, created_by, modified_by, created_at, modified_at)
-values (1, '제목입니다.1', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50'),
-       (1, '제목입니다.1', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54');
+values (1, '제목입니다.11', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50'),
+       (1, '제목입니다.22', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54');
