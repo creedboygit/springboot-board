59de01cf1fd4a4d39d8b40d6cb4c44444c5aec1a
creedboy <polozh@naver.com>
2023-08-16 17:44:35 +0900

    data.sql 댓글 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,7 +11,10 @@
 -- ;
 
 -- 123 게시글
-insert into article (user_id, title, content, created_by, modified_by, created_at, modified_at)
-values
-('creedboy1', '제목입니다.1', '내용입니다.2', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50'),
-('creedboy2', '제목입니다.1', '내용입니다.2', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54');
+insert into article (user_id, title, content, created_by, modified_by, created_at, modified_at, hashtag)
+values ('creedboy1', '제목입니다.1', '내용입니다.2', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50', '해시태그'),
+       ('creedboy2', '제목입니다.1', '내용입니다.2', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54', '해시태그');
+
+insert into article_comment (article_id, content, created_by, modified_by, created_at, modified_at)
+values (1, '제목입니다.1', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50'),
+       (1, '제목입니다.1', 'Arv', 'Keelby', '2021-05-06 11:51:24', '2021-05-23 08:34:54');
