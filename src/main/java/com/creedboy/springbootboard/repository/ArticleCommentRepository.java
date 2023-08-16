5b9e38a16fd84c4d06bc726f7f3f368b6f7da18e
creedboy <polozh@naver.com>
2023-08-16 16:08:12 +0900

    data rest, hal explorer 추가

--- src/main/java/com/creedboy/springbootboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/creedboy/springbootboard/repository/ArticleCommentRepository.java
@@ -2,7 +2,9 @@ package com.creedboy.springbootboard.repository;
 
 import com.creedboy.springbootboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
