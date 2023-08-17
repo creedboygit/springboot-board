64a20eb3ea90120f767cbd48195409336d1389c8
creedboy <polozh@naver.com>
2023-08-17 11:04:07 +0900

    thymeleaf configuration

--- src/main/java/com/creedboy/springbootboard/SpringbootBoardApplication.java
+++ src/main/java/com/creedboy/springbootboard/SpringbootBoardApplication.java
@@ -2,8 +2,10 @@ package com.creedboy.springbootboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 //@EnableJpaRepositories
 //@EnableJpaAuditing
 public class SpringbootBoardApplication {
