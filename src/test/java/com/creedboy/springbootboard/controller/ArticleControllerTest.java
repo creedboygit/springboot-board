8a5b7e5bc654b460ffc6274574c2618436a616fa
creedboy <polozh@naver.com>
2023-08-17 12:45:51 +0900

    security config 적용, 로그인 view 테스트

--- src/test/java/com/creedboy/springbootboard/controller/ArticleControllerTest.java
+++ src/test/java/com/creedboy/springbootboard/controller/ArticleControllerTest.java
@@ -1,16 +1,19 @@
 package com.creedboy.springbootboard.controller;
 
+import com.creedboy.springbootboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @DisplayName("view 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest({ArticleController.class})
 class ArticleControllerTest {
 
@@ -20,7 +23,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 리스트 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -35,7 +37,6 @@ class ArticleControllerTest {
             .andExpect(MockMvcResultMatchers.view().name("articles/index"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
