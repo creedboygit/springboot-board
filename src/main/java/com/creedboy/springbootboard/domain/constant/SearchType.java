58c5506e497208df69e2bac5ed18ca0dd19e7f61
creedboy <polozh@naver.com>
2023-08-22 01:39:42 +0900

    검색 바 - enum 적용

--- src/main/java/com/creedboy/springbootboard/domain/constant/SearchType.java
+++ src/main/java/com/creedboy/springbootboard/domain/constant/SearchType.java
@@ -1,6 +1,26 @@
 package com.creedboy.springbootboard.domain.constant;
 
+import lombok.Getter;
+
+//@RequiredArgsConstructor
+@Getter
 public enum SearchType {
 
-    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+//    TITLE("TITLE", "제목"),
+//    CONTENT("CONTENT", "본문"),
+//    ID("ID", "유저 ID"),
+//    NICKNAME("NICKNAME", "닉네임"),
+//    HASHTAG("HASHTAG", "해시태그");
+
+    TITLE("제목"),
+    CONTENT("본문"),
+    ID("유저 ID"),
+    NICKNAME("닉네임"),
+    HASHTAG("해시태그");
+
+    private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
 }
